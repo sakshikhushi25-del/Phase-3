4631adb4cf370f93dce3e3d93153254b26d413e4
sakshikhushi25-del <sakshikhushi25@navgurukul.org>
2026-02-25 14:07:36 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,12 @@
+document.getElementById("quizBtn").addEventListener("click", () => {
+    window.location.href = "index.html";
+});
+document.getElementById("todoBtn").addEventListener("click", () => {
+    window.location.href = "todo.html";
+});
+document.getElementById("tipBtn").addEventListener("click", () => {
+    window.location.href = "calc.html";
+});
+document.getElementById("quoteBtn").addEventListener("click", () => {
+    window.location.href = "quotes.html";
+});
